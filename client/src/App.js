bdb83deed4d265af2be6ab2dcde7d6e1dc034720
Abhishek Bhatt <abhishekbhatt072003@gmail.com>
2021-10-15 08:51:15 +0530

    created ChannelListContainer.jsx for the sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
+import { StreamChat } from 'stream-chat'
+import { Chat } from "stream-chat-react"
+import Cookies from "universal-cookie"
 import './App.css'
+
+import { ChannelListContainer, ChannelContainer } from "./components"
+
+
+const apiKey = "ahy5jcy3ep29";
+const client = StreamChat.getInstance(apiKey);
+
 
 
 const App = () => {
     return (
-        <div>
+        <div className="app__wrapper">
+            <Chat client={client} theme="team light">
+                <ChannelListContainer
 
+                />
+                <ChannelContainer
+
+                />
+            </Chat>
         </div>
     )
 }
---
+++ client/src/components/ChannelListContainer.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { ChannelList, useChatContext } from 'stream-chat-react'
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from 'stream-chat-react'
+import Cookies from 'universal-cookie'
+import HospitalIcon from "../assets/hospital.png"
+import Logout from "../assets/logout.png"
+
+
+const Sidebar = () => {
+    return (
+        <div className="channel-list__sidebar">
+            <div className="channel-list__sidebar__icon1">
+                <div className="icon1__inner">
+                    <img src={HospitalIcon} alt="Hospital" width="30" />
+                </div>
+            </div>
+
+
+            <div className="channel-list__sidebar__icon2">
+                <div className="icon1__inner">
+                    <img src={Logout} alt="Logout" width="30" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+
+const ChannelListContainer = () => {
+    return (
+        <>
+            <Sidebar />
+        </>
+    )
+}
+
+export default ChannelListContainer
