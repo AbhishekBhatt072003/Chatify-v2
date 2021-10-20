7a2b520fa86851218bd5ac14c1fa60656b34839a
Abhishek Bhatt <abhishekbhatt072003@gmail.com>
2021-10-20 07:45:30 +0530

    created auth and edited team channel preview

---
+++ client/src/components/Auth.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import Cookies from 'universal-cookie'
+
+const Auth = () => {
+    return (
+        <div className="auth__form-container">
+            <div className="auth__form-container_fields">
+                <div >
+                    hello
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Auth
